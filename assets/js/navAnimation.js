fb88b65a90b9c7512febb93017470268f532f183
Matias <matiasrondinellaphoto@gmail.com>
2022-05-12 16:56:19 -0300

    modificaciones varias de estilo

--- assets/js/navAnimation.js
+++ assets/js/navAnimation.js
@@ -1,14 +1,18 @@
 const navAnimation = document.getElementById('navAnimation');
+const navContentAnimation = document.getElementById('navContentAnimation');
 
 
 window.onscroll = function () {
     if (document.documentElement.scrollTop > 100) {
         navAnimation.style.height = '60px';
         navAnimation.style.backgroundColor = '#009ee2ca';
+        navContentAnimation.style.fontSize = '20px';
     } else if (document.documentElement.scrollTop < 100) {
         navAnimation.style.height = '70px';
         navAnimation.style.opacity = '1';
         navAnimation.style.backgroundColor = '#009ee2';
+        navContentAnimation.style.fontSize = '25px';
+        
     } 
 
 }
