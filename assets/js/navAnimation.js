a3cb6c4cd89c259e9eddda9c9a85b3d09ae7c249
Matias <matiasrondinellaphoto@gmail.com>
2022-05-12 15:15:49 -0300

    arregle errores en navAnimation de estilo CSS

--- assets/js/navAnimation.js
+++ assets/js/navAnimation.js
@@ -1,14 +1,16 @@
 const navAnimation = document.getElementById('navAnimation');
 
 
-window.onscroll = function navAnimationF() {
+window.onscroll = function () {
     if (document.documentElement.scrollTop > 100) {
         navAnimation.style.height = '60px';
         navAnimation.style.backgroundColor = '#009ee2ca';
-    }else{
+    } else if (document.documentElement.scrollTop < 100) {
         navAnimation.style.height = '70px';
-    }
-    
+        navAnimation.style.opacity = '1';
+        navAnimation.style.backgroundColor = '#009ee2';
+    } 
+
 }
 
 
